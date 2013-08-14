7c8d22ab0dd2d0ae33da8b48ca7a701ec0155816
Pippin Williamson <pippin@pippinsplugins.com>
2013-08-14 12:45:54 -0500

    Save and selected

--- includes/metabox.php
+++ includes/metabox.php
@@ -22,11 +22,31 @@ function cgc_render_related_download_metabox() {
 	if( ! class_exists( 'Easy_Digital_Downloads' ) )
 		return;
 
+	$selected = get_post_meta( get_the_ID(), 'cgc_related_edd_productt', true ) );
+
 	// Switch to hub site
 	switch_to_blog( 1 );
 
-	echo EDD()->html->product_dropdown();
+	echo EDD()->html->product_dropdown( 'edd_products', $selected );
 
 	restore_current_blog();
 
 }
+
+function cgc_related_download_save( $post_id ) {
+	global $post;
+
+	if ( ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) || ( defined( 'DOING_AJAX') && DOING_AJAX ) || isset( $_REQUEST['bulk_edit'] ) ) return $post_id;
+
+	if ( isset( $post->post_type ) && $post->post_type == 'revision' )
+		return $post_id;
+
+	if ( ! current_user_can( 'edit_post', $post_id ) )
+		return $post_id;
+
+	if( empty( $_POST['edd_products'] ) )
+		delete_post_meta( $post_id, 'cgc_related_edd_product' );
+	else
+		update_post_meta( $post_id, 'cgc_related_edd_product', absint( $_POST['edd_products'] ) );
+}
+add_action( 'save_post', 'cgc_related_download_save' );
